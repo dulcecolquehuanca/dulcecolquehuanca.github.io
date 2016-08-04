69a46d64ad8d491a41f22f9798f30e97b35996de
dulce maria <mariacolquehuanca1992@gmail.com>
2016-08-04 09:49:52 -0500

    avance

---
+++ scrit.js
@@ -0,0 +1,8 @@
+var num =100;
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > num) {
+        $('.menu').addClass('fixed');
+    } else {
+        $('.menu').removeClass('fixed');
+    }
+});
